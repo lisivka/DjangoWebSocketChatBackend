bdcec0c3d0390b9c0a83feda264c15274791b852
lisivka <lisivka@gmail.com>
2024-01-09 00:39:03 +0200

    add exception, username

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,46 +1,95 @@
+from django.contrib.auth import get_user_model
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
 
+User = get_user_model()
+
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        try:
+            self.room_name = self.scope['url_route']['kwargs']['room_name']
+            self.room_group_name = 'chat_%s' % self.room_name
+            async_to_sync(self.channel_layer.group_add)(
+                self.room_group_name,
+                self.channel_name
+            )
+            self.accept()
+            session = self.scope["session"]
+            session_id = session.session_key or session.create()
+            # print(f"{self.scope=}")
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
-        )
+            async_to_sync(self.send(text_data=json.dumps({
+                'event': "Connect",
+                'message': f"Connected to room: {self.room_name}",
+                'session_id': session_id,
+            }))
+            )
 
-        self.accept()
+
+        except Exception as e:
+            async_to_sync(self.send(text_data=json.dumps({
+                'event': "Error",
+                'message': "Error connect:" + str(e),
+            }))
+            )
 
     def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
-        )
+        try:
+            async_to_sync(self.channel_layer.group_discard)(
+                self.room_group_name,
+                self.channel_name
+            )
+            async_to_sync(self.send(text_data=json.dumps({
+                'event': "Connect",
+                'message': f"Disconnected from room: {self.room_name}",
+            }))
+            )
+
+        except Exception as e:
+            async_to_sync(self.send(text_data=json.dumps({
+                'event': "Error",
+                'message': "Error disconnect:" + str(e),
+            }))
+            )
 
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        username = text_data_json['username']
+        try:
+            text_data_json = json.loads(text_data)
+            print(f"{text_data_json=}")
+            message = text_data_json['message']
+            username = text_data_json['username']
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': '[' + self.room_name+ ']' + '[' + username + ']' + " " + message,
+                    'username': username,
+                }
+            )
 
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'username': username
-            }
-        )
+        except Exception as e:
+            async_to_sync(self.send(text_data=json.dumps({
+                'event': "Error",
+                'message': "Error receive:" + str(e),
+            }))
+            )
 
     def chat_message(self, event):
-        message = event['message']
-        username = event['username']
+        try:
+            message = event['message']
+            username = event['username']
+            print(f"{event=}")
 
-        self.send(text_data=json.dumps({
-            'event': "Send",
-            'message': message,
-            'username': username
-        }))
+            self.send(text_data=json.dumps({
+                'event': "Send",
+                'message': message,
+                'username': username,
+            }))
+        except Exception as e:
+            async_to_sync(self.send(text_data=json.dumps({
+                'event': "Error",
+                'message': "Error chat_message:" + str(e),
+            }))
+            )
